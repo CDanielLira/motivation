3a3acb65528f1586551f759599cb5600cf39f633
unknown <danieldelira2949@gmail.com>
2024-08-29 11:37:07 -0600

    fix(auth): Relesed to general page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,49 +12,15 @@ import { CommonModule } from '@angular/common';
 })
 export class AppComponent implements OnInit {
   title = 'Edith Motivation';
-  private audio: HTMLAudioElement | undefined;
-  public audioStarted : boolean = false;
-  public phrases : string [] = [
-    '',
-    'El que haga el examen al final es un plato de sopa',
-    'Despues del examen debemos de ir por unas chaves',
-    'Un pediente menos jaja',
-    'Monty te manda un abrazo, dice que suerte en tu examen',
-    'Relajateee, tranquilaaa',
-    ''
-  ]
-  public phrase !: string;
-
+  
   constructor() {
     
     
   }
 
   ngOnInit(): void {
-    this.phrase = this.phrases[this.getRandomInt(this.phrases.length, 0)];
     
   }
 
-  getRandomInt(min: number, max: number): number {
-    return Math.floor(Math.random() * (max - min + 1)) + min;
-  }
-
-  playSound(): void {
-    if (!this.audio) {
-      this.audio = new Audio('assets/sound.mp3');
-      this.audio.addEventListener('ended', () => {
-        this.audioStarted = false;
-      });
-    }
-    this.audio.play();
-    this.audioStarted = true;
-  }
-
-  stopSound(): void {
-    if (this.audio) {
-      this.audio.pause();
-      this.audio.currentTime = 0; // Opcional: Reinicia el audio al principio
-      this.audioStarted = false;
-    }
-  }
+  
 }
